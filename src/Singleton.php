26826d998934f0417494d99239786cda7fd1ec89
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-09-18 17:25:58 +0300

    first commit

---
+++ src/Singleton.php
@@ -0,0 +1,12 @@
+<?php
+namespace Cvy\DesignPatterns;
+
+class Singleton
+{
+  use tSingleton;
+
+  protected function __construct()
+  {
+    throw new Exception( 'This method is abstract and must be implemented therefore!' );
+  }
+}
